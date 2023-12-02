28becfac37d74262e36ecb73d0c8e69422a90716
Aldis Poga <aldis.poga@gmail.com>
2023-12-02 21:56:06 +0200

    added create and edit

--- 12MD | CRUD React/src/App.tsx
+++ 12MD | CRUD React/src/App.tsx
@@ -1,20 +1,17 @@
-import { Create } from './components/create'
-
-import './App.css'
-
+import { Create } from './components/create';
+import { Edit } from './components/Edit';
+import './App.css';
 
 function App() {
-
   return (
     <>
-    <h1>Driver registry</h1>
-    <div>
-      <Create/>
-    </div>
+      <h1>Driver registry</h1>
+      <div>
+        <Create />
+        <Edit />
+      </div>
     </>
-    
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- 12MD | CRUD React/src/components/Create.tsx
+++ 12MD | CRUD React/src/components/Create.tsx
@@ -1,43 +1,81 @@
-import { useState } from "react"
-import axios from "axios";
+import { useState } from 'react';
+import axios from 'axios';
 
-type Driver = {
-    id: number;
-    name: string;
-    racesWon: number;
-    favTrack: string;
-    teamName: string;
-    photoURL: string;
-    createdAt: Date;
-  };
+export type Driver = {
+  id: number;
+  driverName: string;
+  racesWon: number;
+  favTrack: string;
+  teamName: string;
+  photoURL: string;
+  createdAt: Date;
+};
 
 export const Create = () => {
-    const [driverName, setDriverName] = useState('');
-    const [racesWon, setRacesWon] = useState(0);
-    const [favTrack, setFavTrack] = useState('');
-    const [teamName, setTeamName] = useState('');
-    const [photoUrl, setPhotoUrl] = useState('');
+  const [driverName, setDriverName] = useState('');
+  const [racesWon, setRacesWon] = useState(0);
+  const [favTrack, setFavTrack] = useState('');
+  const [teamName, setTeamName] = useState('');
+  const [photoURL, setPhotoUrl] = useState('');
 
-    const postDriver = () => {
-        axios.post<Driver>('http://localhost:3000/drivers', {
-            name: driverName,
-            racesWon: racesWon,
-            favTrack: favTrack,
-            teamName: teamName,
-            photoURL: photoUrl,
-            createdAt: new Date(),
-        })
-    }
+  const postDriver = () => {
+    axios.post<Driver>('http://localhost:3000/drivers', {
+      driverName: driverName,
+      racesWon: racesWon,
+      favTrack: favTrack,
+      teamName: teamName,
+      photoURL: photoURL,
+      createdAt: new Date(),
+    });
+  };
 
-return (
-    <div className="input-form__wrapper">
-        <form>
-            <input className="input" type="text" name="driverName" placeholder="Full Name" onChange={(e) => setDriverName(e.target.value)} required/>
-            <input className="input" type="number" name="racesWon" placeholder="Races Won" onChange={(e) => setRacesWon(e.target.valueAsNumber)} required/>
-            <input className="input" type="text" name="favTrack" placeholder="Favorite Track" onChange={(e) => setFavTrack(e.target.value)} required/>
-            <input className="input" type="text" name="teamName" placeholder="Team Name" onChange={(e) => setTeamName(e.target.value)} required/>
-            <input className="input" type="text" name="photoUrl" placeholder="Photo URL" onChange={(e) => setPhotoUrl(e.target.value)} required/>
-            <button onClick={postDriver} type="submit">Register driver</button>
-        </form>
+  return (
+    <div className='input-form__wrapper'>
+      <form>
+        <input
+          className='input'
+          type='text'
+          name='driverName'
+          placeholder='Full Name'
+          onChange={(e) => setDriverName(e.target.value)}
+          required
+        />
+        <input
+          className='input'
+          type='number'
+          name='racesWon'
+          placeholder='Races Won'
+          onChange={(e) => setRacesWon(e.target.valueAsNumber)}
+          required
+        />
+        <input
+          className='input'
+          type='text'
+          name='favTrack'
+          placeholder='Favorite Track'
+          onChange={(e) => setFavTrack(e.target.value)}
+          required
+        />
+        <input
+          className='input'
+          type='text'
+          name='teamName'
+          placeholder='Team Name'
+          onChange={(e) => setTeamName(e.target.value)}
+          required
+        />
+        <input
+          className='input'
+          type='text'
+          name='photoUrl'
+          placeholder='Photo URL'
+          onChange={(e) => setPhotoUrl(e.target.value)}
+          required
+        />
+        <button onClick={postDriver} type='button'>
+          Register driver
+        </button>
+      </form>
     </div>
-)}
+  );
+};
